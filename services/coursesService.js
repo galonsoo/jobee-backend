d90994074793b4424c94cb74036289ee19abaabd
Ezequiel-P1612 <perezequiel972@gmail.com>
2025-09-29 08:51:11 -0300

    fix: rename parameter in getCourseById function for clarity

--- services/coursesService.js
+++ services/coursesService.js
@@ -24,8 +24,8 @@ async function getAllCourses() {
 }
 
 // Obtener curso por ID
-async function getCourseById(id) {
-        const [rows] = await db.query("SELECT * FROM cursos WHERE id = ?", [id]);
+async function getCourseById(courseId) {
+        const [rows] = await db.query("SELECT * FROM cursos WHERE id = ?", [courseId]);
     if (rows.length === 0) throw new Error("Curso no encontrado");
     return rows[0];
 }
